cda385ef45fba2be673ea65996746d4895f33d98
CameruptQDX <38517601+CameruptQDX@users.noreply.github.com>
2019-12-16 15:08:00 -0500

    park metatiles

--- data/tilesets/park_collision.asm
+++ data/tilesets/park_collision.asm
@@ -13,7 +13,7 @@
 	tilecoll WALL, WALL, WALL, WALL ; 0c
 	tilecoll WALL, WALL, WALL, WALL ; 0d
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 0e
-	tilecoll WALL, FLOOR, FLOOR, FLOOR ; 0f
+	tilecoll FLOOR, WALL, FLOOR, FLOOR ; 0f
 	tilecoll WALL, WALL, WALL, FLOOR ; 10
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 11
 	tilecoll WALL, WALL, FLOOR, WALL ; 12
@@ -62,3 +62,67 @@
 	tilecoll WALL, WALL, WALL, WALL ; 3d
 	tilecoll WALL, WALL, WALL, WALL ; 3e
 	tilecoll LONG_GRASS, LONG_GRASS, LONG_GRASS, LONG_GRASS ; 3f
+	tilecoll WALL, WALL, WALL, WALL ; 40
+	tilecoll LONG_GRASS, WALL, LONG_GRASS, WALL ; 41
+	tilecoll WALL, LONG_GRASS, WALL, LONG_GRASS ; 42
+	tilecoll LONG_GRASS, LONG_GRASS, WALL, WALL ; 43
+	tilecoll WALL, WALL, FLOOR, FLOOR ; 44
+	tilecoll WALL, WALL, LONG_GRASS, LONG_GRASS ; 45
+	tilecoll WALL, FLOOR, WALL, FLOOR ; 46
+	tilecoll FLOOR, WALL, FLOOR, WALL ; 47
+	tilecoll FLOOR, FLOOR, WALL, WALL ; 48
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 49
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 4a
+	tilecoll WALL, FLOOR, WALL, FLOOR ; 4b
+	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 4c
+	tilecoll FLOOR, FLOOR, WALL, WALL ; 4d
+	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 4e
+	tilecoll FLOOR, WALL, FLOOR, WALL ; 4f
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 50
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 51
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 52
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 53
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 54
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 55
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 56
+	tilecoll WALL, FLOOR, WALL, FLOOR ; 57
+	tilecoll FLOOR, WALL, FLOOR, WALL ; 58
+	tilecoll WALL, WALL, FLOOR, FLOOR ; 59
+	tilecoll WALL, LONG_GRASS, WALL, LONG_GRASS ; 5a
+	tilecoll LONG_GRASS, WALL, LONG_GRASS, WALL ; 5b
+	tilecoll WALL, WALL, DOOR, WALL ; 5c
+	tilecoll , , ,  ; 5d
+	tilecoll , , ,  ; 5e
+	tilecoll , , ,  ; 5f
+	tilecoll , , ,  ; 60
+	tilecoll , , ,  ; 61
+	tilecoll , , ,  ; 62
+	tilecoll , , ,  ; 63
+	tilecoll , , ,  ; 64
+	tilecoll , , ,  ; 65
+	tilecoll , , ,  ; 66
+	tilecoll , , ,  ; 67
+	tilecoll , , ,  ; 68
+	tilecoll , , ,  ; 69
+	tilecoll , , ,  ; 6a
+	tilecoll , , ,  ; 6b
+	tilecoll , , ,  ; 6c
+	tilecoll , , ,  ; 6d
+	tilecoll , , ,  ; 6e
+	tilecoll , , ,  ; 6f
+	tilecoll , , ,  ; 70
+	tilecoll , , ,  ; 71
+	tilecoll , , ,  ; 72
+	tilecoll , , ,  ; 73
+	tilecoll , , ,  ; 74
+	tilecoll , , ,  ; 75
+	tilecoll , , ,  ; 76
+	tilecoll , , ,  ; 77
+	tilecoll , , ,  ; 78
+	tilecoll , , ,  ; 79
+	tilecoll , , ,  ; 7a
+	tilecoll , , ,  ; 7b
+	tilecoll , , ,  ; 7c
+	tilecoll , , ,  ; 7d
+	tilecoll , , ,  ; 7e
+	tilecoll , , ,  ; 7f
