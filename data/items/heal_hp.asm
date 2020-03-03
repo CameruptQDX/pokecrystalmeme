62258fe6e59fb5038df33242f9f38f1636ce6c55
CameruptQDX <38517601+CameruptQDX@users.noreply.github.com>
2020-03-02 19:15:45 -0500

    beat the item nerfs back a bit

--- data/items/heal_hp.asm
+++ data/items/heal_hp.asm
@@ -3,14 +3,14 @@ HealingHPAmounts:
 	dbw SODA_POP,      40
 	dbw LEMONADE,      60
 	dbw HYPER_POTION, 120
-	dbw SUPER_POTION,  40
+	dbw SUPER_POTION,  50
 	dbw POTION,        20
 	dbw MAX_POTION,   MAX_STAT_VALUE
 	dbw FULL_RESTORE, MAX_STAT_VALUE
 	dbw MOOMOO_MILK,   80
 	dbw BERRY,         10
 	dbw GOLD_BERRY,    30
-	dbw ENERGYPOWDER,  40 ; same as super potion
+	dbw ENERGYPOWDER,  50 ; same as super potion
 	dbw ENERGY_ROOT,  120 ; same as hyper potion
 	dbw RAGECANDYBAR,  40 ; a little better
 	dbw BERRY_JUICE,   20
